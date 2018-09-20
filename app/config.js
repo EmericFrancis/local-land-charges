fe83c66f95af472eed8b9f5333435f2e8cf805c0
Emeric Francis <Emeric.Francis@landregistry.gov.uk>
2018-09-20 09:38:52 +0100

    created start page and added shame scss

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Search for local land charges',
 
   // Default port that prototype runs on
   port: '3000',
